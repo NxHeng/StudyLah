3b5d35663ab3cc73165106d7fe4a1402083c6124
Alvin Lee <alvinlkflee75@gmail.com>
2023-06-18 17:08:14 +0800

    Profile styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,9 +21,9 @@
 
 <body>
     <div id="app" class="content">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm sticky-top">
+        <nav class="navbar navbar-expand-md bg-color shadow-sm sticky-top">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand h5 mt-2" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -32,19 +32,19 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse mt-2" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
+                        <li class="nav-item h5">
                             <a class="nav-link" href="{{ route('home') }}">HomePage</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item h5">
                             <a class="nav-link" href="{{ route('events.index') }}">EventUp</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item h5">
                             <a class="nav-link" href="{{ route('notes.index') }}">NoteHub</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item h5">
                             <a class="nav-link" href="{{ route('decks.index') }}">FlashCards</a>
                         </li>
                     </ul>
@@ -54,18 +54,18 @@
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
-                                <li class="nav-item">
+                                <li class="nav-item h5">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
-                                <li class="nav-item">
+                                <li class="nav-item h5">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
+                            <li class="nav-item dropdown h5">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
@@ -97,7 +97,7 @@
         </main>
     </div>
 
-    <footer id="footer" class="footer p-5 bg-body-secondary text-dark align-items-center footer align-bottom">
+    <footer id="footer" class="footer p-5 text-dark align-items-center footer align-bottom bg-color">
         <div class="row">
             <div class="col-lg-4 col-sm-6 pt-2">
                 <a class="title h2" href="{{ url('/') }}" style="text-decoration: none">StudyLah</a>
